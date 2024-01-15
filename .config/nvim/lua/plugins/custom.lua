4ae4d70f961a54dd7914431088aa3226f67c3695
fx <1096015776@qq.com>
2024-01-15 19:46:59 +0800

    fix:nvim add mason lsp

--- .config/nvim/lua/plugins/custom.lua
+++ .config/nvim/lua/plugins/custom.lua
@@ -16,6 +16,19 @@ return {
     opts = {
       current_line_blame = true
     }
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "biome",
+        "flake8",
+        "lua-language-server",
+        "shfmt",
+        "stylua",
+        "vetur-vls"
+      },
+    }
   }
 
 }

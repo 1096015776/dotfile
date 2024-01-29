d5a21c099520c18ce9325e76da560d4fbab4fc35
fx <1096015776@qq.com>
2024-01-29 14:38:14 +0800

    fix:use default map and options and disable auto format

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,16 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local map = vim.keymap.set
-map({ 'n', 'v' }, 'H', '^')
-map({ 'n', 'v' }, 'L', 'g_')
-map('i', 'jk', '<esc>')
+-- map({ "n", "v" }, "H", "^")
+-- map({ "n", "v" }, "L", "g_")
+map("i", "jk", "<esc>")
+map("v", "<c-c>", '"*y')
+
+-- tmux
+local nvim_tmux_nav = require("nvim-tmux-navigation")
+map("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+map("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+map("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+map("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+map("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+map("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)

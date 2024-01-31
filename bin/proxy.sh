c9eaf5628120a18022093dee601564c0fd426f4a
fx <1096015776@qq.com>
2024-01-31 14:23:38 +0800

    feature:add proxy shell

---
+++ bin/proxy.sh
@@ -0,0 +1,11 @@
+proxy_ip=$(cat /etc/resolv.conf|grep nameserver |awk -F 'nameserver' '{print $2 }')
+proxy_on ()
+{
+  echo 'start proxy'
+  export https_proxy="http://${proxy_ip}:33210" http_proxy="http://${proxy_ip}:33210" all_proxy="socks5://${proxy_ip}:33211"
+}
+un_proxy(){
+  unset https_proxy http_proxy all_proxy
+}
+proxy_on
+

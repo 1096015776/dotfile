6c99ae2f10ac43608ddd50e99eda73c83af3e78b
fx <1096015776@qq.com>
2024-01-31 14:32:48 +0800

    move file

--- bin/proxy.zsh
+++ bin/proxy.zsh
@@ -3,6 +3,7 @@ proxy_on ()
 {
   echo 'start proxy'
   export https_proxy="http://${proxy_ip}:33210" http_proxy="http://${proxy_ip}:33210" all_proxy="socks5://${proxy_ip}:33211"
+  echo $http_proxy
 }
 un_proxy(){
   unset https_proxy http_proxy all_proxy
